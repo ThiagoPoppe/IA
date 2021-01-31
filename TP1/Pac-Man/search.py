70059fb51c0ae6f867a4d14c3728401cfa8d1176
ThiagoPoppe <thiagopoppe@dcc.ufmg.br>
2021-01-31 19:58:27 -0200

    Passo 3 concluído

--- TP1/Pac-Man/search.py
+++ TP1/Pac-Man/search.py
@@ -120,7 +120,34 @@ def depthFirstSearch(problem):
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    
+    openList = util.Queue()
+    exploredSet = set()
+
+    # Manually inserting starting node
+    start = {
+        'state': problem.getStartState(),
+        'actions': []
+    }
+    openList.push(start)
+
+    while not openList.isEmpty():
+        node = openList.pop()
+        if node['state'] not in exploredSet:
+            exploredSet.add(node['state'])
+
+            if problem.isGoalState(node['state']):
+                return node['actions']
+
+            for successor in problem.getSuccessors(node['state']):
+                newNode = {
+                    'state': successor[0],
+                    'actions': node['actions'] + [successor[1]]
+                }
+                openList.push(newNode)
+
+    print('*** Unable to find solution ***')
+    return []
 
 
 def uniformCostSearch(problem):
